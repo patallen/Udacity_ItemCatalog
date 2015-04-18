7561c0c07d2f6c513c4a5a49bd781cd448a344ef
Patrick Allen <prallen90@gmail.com>
2015-04-18 02:27:26 -0400

    Database models and begin fill script

---
+++ app/models.py
@@ -0,0 +1,50 @@
+from app import db
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+
+    id = db.Column(db.String(64), primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(500))
+    picture = db.Column(db.String(250))
+
+    def __init__(self, name, picture=None):
+        if picture is None:
+            self.picture = ''
+        else:
+            self.picture = picture
+        self.name = name
+        self.id = name.lower()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    picture = db.Column(db.String(250))
+
+    def __init__(self, name, email, picture=None):
+        self.name = name
+        self.email = email
+        if picture is None:
+            self.picture = ''
+        else:
+            self.picture = picture
+
+
+class Game(db.Model):
+    __tablename__ = 'game'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    picture = db.Column(db.String(250))
+
+    genre_id = db.Column(db.String(64), db.ForeignKey('genre.id'))
+    genre = db.relationship(Genre)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
---
+++ fill_db.py
@@ -0,0 +1,55 @@
+from app import db
+from app.models import User, Game, Genre
+
+db.create_all()
+
+action = Genre('Action')
+action.description = 'Fast paced and invigorating'
+db.session.add(action)
+
+adventure = Genre('Adventure')
+adventure.description = 'Find your way, or die.'
+db.session.add(adventure)
+
+arcade = Genre('Arcade')
+arcade.description = 'Oldschool games for casual gamers'
+db.session.add(arcade)
+
+fighting = Genre('Fighting')
+fighting.description = 'Hand-to-hand combat.'
+db.session.add(fighting)
+
+fps = Genre('FPS')
+fps.description = 'First-person shooter'
+db.session.add(fps)
+
+puzzle = Genre('Puzzle')
+puzzle.description = 'Mind-boggling games for the braniacs'
+db.session.add(puzzle)
+
+sim = Genre('Simulation')
+sim.description = 'From flight to city, this is real life.'
+db.session.add(sim)
+
+sports = Genre('Sports')
+sports.description = 'Based off of real life sports!'
+db.session.add(sports)
+
+strategy = Genre('Strategy')
+strategy.description = 'Plan your next move carefully.'
+db.session.add(strategy)
+
+pat = User('Pat', 'prallen90@gmail.com')
+db.session.add(pat)
+
+gta5 = Game(name = 'Grand Theft Auto V', description = 'Grand Theft Auto V is an action-adventure game played from either a first-person or third-person view.', genre_id='action', user_id=1)
+db.session.add(gta5)
+
+deadisland = Game(name = 'Dead Island', description = 'Zombie Apocolypse. Can you survive the hordes of undead?', genre_id='action', user_id=1)
+db.session.add(deadisland)
+
+bioshock = Game(name = 'BioShock', description = 'Set in 1960 in the underwater city of Rapture, with the history of the city told through audio recordings the player can collect.', genre_id='fps', user_id=1)
+db.session.add(bioshock)
+
+
+db.session.commit()
