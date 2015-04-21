ce03871b24760834fd430448ccd2021930b37eee
Patrick Allen <prallen90@gmail.com>
2015-04-21 16:57:12 -0400

    Added upload form

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import Form
 from app import db
 from app.models import Genre
-from wtforms import SelectField, StringField, TextAreaField, SubmitField
+from wtforms import SelectField, StringField, TextAreaField, SubmitField, FileField
 from wtforms.validators import DataRequired
 
 
 class GameForm(Form):
     title = StringField('Title', validators=[DataRequired()])
     genre = SelectField('Genre', validators=[DataRequired()])
+    image = FileField('Image')
     description = TextAreaField('Description', validators=[DataRequired()])
     
 
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 DEBUG = True
 SECRET_KEY = "Not so secret, eh?"
 SQLALCHEMY_DATABASE_URI = "sqlite:///../gamecatalog.db"
+
+UPLOAD_FOLDER = "app/media/images/games"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
